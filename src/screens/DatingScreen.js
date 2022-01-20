2cac63c13586ac1c09582792c212e510fb2a5508
JinTakLee <takitaki228@gmail.com>
2022-01-20 23:24:06 +0900

    Very Rough Design of Dating Screen

---
+++ src/screens/DatingScreen.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const DatingScreen = ( {navigation} ) => {
+
+    return (
+      <View style={styles.wrapper}>
+        <View style={styles.container}>
+            <View style={[styles.upper_box, styles.mypic]}></View>
+            <View style={[styles.upper_box, styles.bonus]}></View>
+        </View>
+        <View style={styles.container2}>
+            <View style={[styles.mid_box, styles.pbox]}></View>
+            <View style={[styles.mid_box, styles.pbox]}></View>
+            <View style={[styles.mid_box, styles.pbox]}></View>
+            <View style={[styles.mid_box, styles.pbox]}></View>            
+        </View>
+        <View style={styles.container3}>
+            <View style={[styles.box, styles.more]}></View>
+
+        </View>        
+      </View>
+    );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  container: {
+    flex: .2,
+    flexDirection: 'row',
+    //borderBottomWidth: 1,
+    //borderBottomColor: '#000',
+    justifyContent: 'space-between', 
+    padding: 10
+  },
+  container2: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between', //replace with flex-end or center
+    padding:10,
+    //alignItems: 'flex-start' //replace with flex-end or center
+  },  
+  container3: {
+    flex: .2,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center' //replace with flex-end or center
+  },
+  upper_box: {
+    flex: 1,
+    height: 60,
+    backgroundColor: 'red',
+    margin:10
+  },  
+  mid_box: {
+    flex: 1,
+    height: 60,
+    margin:5
+  },    
+  box: {
+    flex: 1,
+    height: 100,
+    margin:10
+  },
+  bottom_box: {
+    flex: 1,
+    margin:10
+  },
+  mypic :{
+    backgroundColor: '#2196F3',
+    flex : 0.2
+  },
+  more: {
+    backgroundColor: 'yellow',
+    flex : 1,
+    height : 30
+  },  
+  bonus: {
+    backgroundColor: '#8BC34A'
+  },
+
+  bonus: {
+    backgroundColor: '#e3aa1a',
+    flex : 0.2
+  },
+  pbox: {
+    backgroundColor: 'purple',
+    flex : 0.3
+  },
+  two: {
+    flex: 1.5,
+  },
+
+  mainView: {
+    flex: 1,
+    //display: "flex",
+    //flexDirection: "row-reverse",
+    //flexWrap: "wrap",
+    //alignContent: "space-between",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  textstyle: {
+    fontSize: 25,
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+
+  touchableView :{
+    marginBottom: 30,
+    width :150,
+    height : 50,
+    //display: "flex",
+    //flexWrap: "wrap",
+    //alignContent: "space-between",    
+    alignItems : 'center',
+    backgroundColor : 'orange',
+    borderWidth : 5,
+    borderRadius: 10
+  },
+
+  touchableText :{
+    textAlign : 'center',
+    paddingTop : 5,
+    fontSize : 20, 
+    fontWeight : 'bold',
+    color : 'white'
+  }
+});
+
+export default DatingScreen;
